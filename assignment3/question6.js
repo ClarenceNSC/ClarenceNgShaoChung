d097c496e2e35147747fe625cfb0a17acf0b2742
ClarenceNSC <clarence_ng@outlook.com>
2025-02-28 17:54:07 +0800

    c

--- assignment3/question6.js
+++ assignment3/question6.js
@@ -1,9 +1,30 @@
 const disneyData = {
-	open: "120.54",
-	high: "122.56",
-	low: "120.54",
-	close: "121.09",
-	volume: "90000"
-};
+    open: "120.54",
+    high: "122.56",
+    low: "120.54",
+    close: "121.09",
+    volume: "90000"
+}
+const openPx = parseFloat(disneyData.open);
+const closePx = parseFloat(disneyData.close);
+const volume = parseInt(disneyData.volume);
+if (closePx > openPx) 
+    if (volume > 100000) {
+        console.log ("Strong Bullish");
+    }
+        else {
+            console.log ("Bullish");
+        }
+    
+    else if (closePx < openPx) 
+            if (volume > 10000) {
+            console.log ("Strong Bearish");
+        }
+            else {
+                console.log ("Bearish");
+            }
+            
+            else {
+                console.log ("neutral")
+            }
 
-// Write your code below
