0ad030af59af02721e3d3562c212d58754619f4a
ClarenceNSC <clarence_ng@outlook.com>
2025-02-28 17:40:27 +0800

    qn3

--- assignment3/question3.js
+++ assignment3/question3.js
@@ -4,5 +4,21 @@ const disneyData = {
 	low: "120.54",
 	close: "121.09"
 };
+const open = parseFloat(disneyData.open);
+const high = parseFloat(disneyData.high);
+const low = parseFloat(disneyData.low);
+const close = parseFloat(disneyData.close);
+const result = open - high + low - close
+console.log (result)
+const resultTofixed = result.toFixed(2)
+console.log (resultTofixed)
 
-// Write your code below
+const disneyData = {
+    open: "120.54",
+    high: "122.56",
+    low: "120.54",
+    close: "121.09",
+    volume: "1512311"
+}
+console.log (disneyData)
+
